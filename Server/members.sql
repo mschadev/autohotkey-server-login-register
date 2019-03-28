a2e184e3c2cc602eeaa919b46c2b88d47f4c08f8
Plorence <master@plorence.kr>
2019-03-28 16:59:03 +0900

    테이블 SQL
    테이블 SQL

---
+++ Server/members.sql
@@ -0,0 +1,47 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- 생성 시간: 19-03-28 16:57
+-- 서버 버전: 5.7.25
+-- PHP 버전: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `a`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `members`
+--
+
+CREATE TABLE `members` (
+  `joinDateTime` datetime NOT NULL,
+  `id` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` tinytext NOT NULL,
+  `resetToken` varchar(255) DEFAULT NULL,
+  `resetComplete` tinyint(1) DEFAULT NULL,
+  `ComputerName1` varchar(255) NOT NULL DEFAULT 'NULL',
+  `ComputerHard1` varchar(255) DEFAULT 'NULL',
+  `License` varchar(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
